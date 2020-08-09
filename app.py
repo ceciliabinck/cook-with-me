a08bba275cbcb3c4bec483a62b8d429a0b61d3ef
ceciliabinck <cecilia4binck@gmail.com>
2020-08-09 13:41:48 +0000

    Inserted delete route

--- app.py
+++ app.py
@@ -71,6 +71,12 @@ def update_recipe(recipe_id):
     return redirect(url_for('get_recipes'))
 
 
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_recipes'))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP", "0.0.0.0"),
             port=os.environ.get("PORT", "5000"),
