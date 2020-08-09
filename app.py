88e732850e1d7dec39261ce1907fce240a4ea835
ceciliabinck <cecilia4binck@gmail.com>
2020-08-09 19:29:42 +0000

    Redirect to profile

--- app.py
+++ app.py
@@ -41,6 +41,8 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registraion Succesfull")
+        return redirect(url_for('profile', username=session["user"]))
+
     return render_template('register.html')
 
 
@@ -57,6 +59,8 @@ def register():
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get('password').lower()
                     flash("Welcome {}", format(request.form.get('username')))
+                    return redirect(url_for(
+                        'profile', username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
