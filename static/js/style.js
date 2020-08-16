4490acb4a06f418ae8fbf5d34971b87c3a0af264
ceciliabinck <cecilia4binck@gmail.com>
2020-08-16 20:11:59 +0000

    Add extra input fields edit/add_recipe

--- static/js/style.js
+++ static/js/style.js
@@ -1,9 +1,95 @@
 $(document).ready(function(){
     $('select').formSelect();
     $('.sidenav').sidenav();
+     $('.tabs').tabs();
+    $('.carousel.carousel-slider').carousel();
+});
+
+/*
+** Add extra field ingredients in add_recipe
+*/
+$(document).ready(function() {
+	var max_fields      = 25; //maximum input boxes allowed
+	var wrapper   		= $(".input_field_wrap"); //Fields wrapper
+	var add_button      = $(".adding_field_button"); //Add button ID
+	
+	var x = 1; //initlal text box count
+	$(add_button).click(function(e){ //on add input button click
+		e.preventDefault();
+		if(x < max_fields){ //max input box allowed
+			x++; //text box increment
+			$(wrapper).append('<div><input type="text" name="ingredients[]"/><a href="#" class="delete_field">Remove</a></div>'); //add input box
+		}
+	});
+	
+	$(wrapper).on("click",".delete_field", function(e){ //user click on remove text
+		e.preventDefault(); $(this).parent('div').remove(); x--;
+	})
+});
+
+/*
+** Add extra field ingredients in edit_recipe
+*/
+$(document).ready(function() {
+	var max_fields      = 25; //maximum input boxes allowed
+	var wrapper   		= $(".input_field_wraps"); //Fields wrapper
+	var add_button      = $(".addings_field_button"); //Add button ID
+	
+	var x = 1; //initlal text box count
+	$(add_button).click(function(e){ //on add input button click
+		e.preventDefault();
+		if(x < max_fields){ //max input box allowed
+			x++; //text box increment
+			$(wrapper).append('<div><input type="text" name="ingredients[]"/><a href="#" class="delete_field">Remove</a></div>'); //add input box
+		}
+	});
+	
+	$(wrapper).on("click",".delete_field", function(e){ //user click on remove text
+		e.preventDefault(); $(this).parent('div').remove(); x--;
+	})
+});
+
+/*
+** Add extra field method in add_recipe
+*/
+$(document).ready(function() {
+	var max_fields      = 25; //maximum input boxes allowed
+	var wrapper   		= $(".input_fields_wrap"); //Fields wrapper
+	var add_button      = $(".add_field_button"); //Add button ID
+	
+	var x = 1; //initlal text box count
+	$(add_button).click(function(e){ //on add input button click
+		e.preventDefault();
+		if(x < max_fields){ //max input box allowed
+			x++; //text box increment
+			$(wrapper).append('<div><input type="text" name="method[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+		}
+	});
+	
+	$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+		e.preventDefault(); $(this).parent('div').remove(); x--;
+	})
 });
 
 
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true
-  });
+/*
+** Add extra field method in edit_recipe
+*/
+$(document).ready(function() {
+	var max_fields      = 25; //maximum input boxes allowed
+	var wrapper   		= $(".input_fields_wraps"); //Fields wrapper
+	var add_button      = $(".adds_field_button"); //Add button ID
+	
+	var x = 1; //initlal text box count
+	$(add_button).click(function(e){ //on add input button click
+		e.preventDefault();
+		if(x < max_fields){ //max input box allowed
+			x++; //text box increment
+			$(wrapper).append('<div><input type="text" name="method[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+		}
+	});
+	
+	$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+		e.preventDefault(); $(this).parent('div').remove(); x--;
+	})
+});
